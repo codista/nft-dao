84dff2bc0992faed88fc436758815350108cdded
codista <nirh30@hotmail.com>
2021-11-25 17:16:13 +0200

    improve scoring

--- nft-score-api/src/opensea.ts
+++ nft-score-api/src/opensea.ts
@@ -4,7 +4,13 @@ import { Asset,OrderSide,Order, OpenSeaFungibleToken } from "opensea-js/lib/type
 import BigNumber from 'bignumber.js';
 import * as OpenseaAPI from "./opensea_api";
 var jp = require('jsonpath');
+import { maxHeaderSize } from 'http';
 const DEC_PRECISION = 4;
+const WEIGHT_SALES = 0.2;
+const WEIGHT_BIDS = 0.6;
+const WEIGHT_TRANSFERS = 0.1;
+const WEIGHT_PERIOD = 0.05;
+const WEIGHT_RECENCY = 0.05;
 
 var jp = require('jsonpath');
 
@@ -76,15 +82,31 @@ export async function getNFTValue(contractAdr: string, tokenID: string): Promise
 
 export async function getExpertScore(expertAddress: string): Promise<any> {
 
-  let data = await OpenseaAPI.openseaApiGet("events",{account_address: expertAddress});
-  if (data===false)
-  { return false;}
-  return calcExpertScoreFromEvents(data);
+  let data=null;
+  let mainnetScore=0;
+  let rinkebyScore=0;
+  let dataMainnet = await OpenseaAPI.openseaApiGet('mainnet',"events",{account_address: expertAddress});
+  let dataRinkeby = await OpenseaAPI.openseaApiGet('rinkeby',"events",{account_address: expertAddress});
+  if (dataMainnet===false && dataRinkeby===false) {
+    return false;
+  }
+
+  if (dataMainnet!==false) {
+    mainnetScore=calcExpertScoreFromEvents(dataMainnet);
+  }
+  if (dataRinkeby!==false) {
+    rinkebyScore=calcExpertScoreFromEvents(dataRinkeby);
+  }
+
+  console.log(`mainnet score: ${mainnetScore}, rinkeby score: ${rinkebyScore}`);
+  return Math.max(mainnetScore,rinkebyScore);
   
 }
 
 function calcExpertScoreFromEvents(data: Object): number
 {
+  
+  let score = 0,periodScore=0, recencyScore=0;
   let sales = jp.query(data,'$.asset_events[?(@.event_type=="successful")]');
   let canceledAuctions = jp.query(data,'$.asset_events[?(@.event_type=="cancelled")]');
   let bids = jp.query(data,'$.asset_events[?(@.event_type=="bid_entered")]');
@@ -92,21 +114,57 @@ function calcExpertScoreFromEvents(data: Object): number
   let transfers = jp.query(data,'$.asset_events[?(@.event_type=="transfer")]');
   let offers = jp.query(data,'$.asset_events[?(@.event_type=="offer_entered")]');
   let approvals = jp.query(data,'$.asset_events[?(@.event_type=="approve")]');
+   
+  
+  //take into consideration how long the address is active trading NFTs and how recent is the experience
+   let transactionDates =  jp.query(data,'$.asset_events[*].transaction.timestamp');
+   let minDate=2147483647000,maxDate=0;
+   transactionDates.forEach((dateString: any)=>{
+        let timestamp=Date.parse(dateString);
+        if (!isNaN(timestamp)) {
+          if (timestamp<minDate) {
+            minDate=timestamp;
+          }
+          if (timestamp>maxDate) {
+            maxDate=timestamp;
+          }
+        }
+   });
 
-  console.log(` sales: ${sales.length} canceledAuctions: ${canceledAuctions.length} bids: ${bids.length} bidWithrawls: ${bidWithrawls.length} transfers: ${transfers.length} offers: ${offers.length} approvals: ${approvals.length} `);
+   var mindateObj = new Date(minDate);
+   var maxdateObj = new Date(maxDate);
+   console.log(` sales: ${sales.length} canceledAuctions: ${canceledAuctions.length} bids: ${bids.length} bidWithrawls: ${bidWithrawls.length} transfers: ${transfers.length} offers: ${offers.length} approvals: ${approvals.length} min date: ${mindateObj} max date: ${maxdateObj}`);
 
-  //console.log(`1: ${sigmoid(1)}, 0: ${sigmoid(0)}, 10: ${sigmoid(10)}, 100: ${sigmoid(100)}, 20: ${sigmoid(20)}, 5: ${sigmoid(5)}, `);
-  //console.log(` sales: ${JSON.stringify(sales)} `);
-  let score = Math.round(sigmoid(5*sales.length+bids.length+transfers.length+offers.length+approvals.length)*10000);
+  if (minDate!=2147483647000 && maxDate!=0)
+   {
+      let period = Math.round((maxDate-minDate)/(1000*60*60*24));
+      let recency=Math.round((Date.now()-maxDate)/(1000*60*60*24));
+    
+      periodScore = Math.round(sigmoid(period,50)*10000);
+      recencyScore = Math.round((1-sigmoid(recency,50))*10000);
 
-  //score should never be zero to enable default solidity score (0) to represent uninitiated score
-  if (score==0) {score=1}
+   }
+  
+  
+
+  //"normalize" each factor using a sigmoid function (since we don't know the range), and then calculate a weighted average of factors)
+  let scoreSales = Math.round(sigmoid(sales.length,2)*10000);
+  let scoreBids = Math.round(sigmoid(bids.length,6)*10000);
+  let scoreTransfers = Math.round(sigmoid(transfers.length,5)*10000);
+  
+  score = Math.round(WEIGHT_SALES*scoreSales + WEIGHT_BIDS*scoreBids + WEIGHT_TRANSFERS*scoreTransfers + WEIGHT_PERIOD*periodScore + WEIGHT_RECENCY*recencyScore);
+   
+  //score should never be zero 
+  //so we start with 1 (1000 on a scale of 1 to 10000 which is the precision on the contract); 
   console.log(`score: ${score}`);
+  if (score<1000) {score=1000}
+  console.log(`score: ${score}, scoreSales: ${scoreSales},scoreBids: ${scoreBids},scoreTransfers: ${scoreTransfers},periodScore: ${periodScore}, recencyScore: ${recencyScore}`);
 
   return score;
 }
 
-const k = 20;
-function sigmoid(z: number) {
+//variant of sigmoid that spreads the results between 0 and 1 (instead of 0.5 to 1)
+//k expands the active range so that we don't get all ones from relatively low values
+function sigmoid(z: number, k: number) {
   return ((1 / (1 + Math.exp(-z/k)))-0.5)*2;
 }
--- nft-score-api/src/opensea_api.ts
+++ nft-score-api/src/opensea_api.ts
@@ -3,24 +3,32 @@ const { curly } = require("node-libcurl");
 //import Curl from "node-libcurl";
 //import * as querystring from "querystring";
 
-const OPENSEA_BASE_URL = "https://rinkeby-api.opensea.io/api/v1/";
-//const OPENSEA_BASE_URL = "https://api.opensea.io/api/v1/";
+const OPENSEA_BASE_URL_RINKEBY = "https://rinkeby-api.opensea.io/api/v1/";
+const OPENSEA_BASE_URL_MAINNET = "https://api.opensea.io/api/v1/";
 
-export async function openseaApiGet(service: string,params: Object ): Promise<Object | false> {
+export type Networks = 'rinkeby'|'mainnet';
+
+export async function openseaApiGet(network: Networks, service: string,params: Object ): Promise<Object | false> {
     //const curlOpensea = new Curl();
-    let url = OPENSEA_BASE_URL+service;
+    let baseURL = (network=='rinkeby')?OPENSEA_BASE_URL_RINKEBY:(network=='mainnet')?OPENSEA_BASE_URL_MAINNET:'';
+    if (baseURL=='') {
+        console.error('unsupported network in opensea_api');
+        return false;
+    }
+    let url = baseURL+service;
     let queryString = querystring.stringify(params);
     url=url+"/?"+queryString;
     const { statusCode, data, headers } = await curly.get(url, {
         //postFields: JSON.stringify(params),
         httpHeader: [
             'Content-Type: application/json',
-            'Accept: application/json'
+            'Accept: application/json',
+            /*'x-api-key: apiKey', */
         ],
         })
     //console.log(statusCode+" "+JSON.stringify(data)+" "+url);
     if (statusCode!=200) {
-        console.error("failed to retrieve info from opensea api ",statusCode);
+        console.error("failed to retrieve info from opensea api "+network,statusCode);
         return false;
     }
     return data;                    
