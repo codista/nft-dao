db3f2dcd44369709b03f291200cf772f5a7246c4
codista <nirh30@hotmail.com>
2021-11-28 09:24:56 +0200

    pre-deploy

--- nft-dao-dapp/scripts/deployDecNFTDao.ts
+++ nft-dao-dapp/scripts/deployDecNFTDao.ts
@@ -4,6 +4,7 @@ import * as dotenv from "dotenv";
 import * as confs from "./../test/conf";
 import fs from "fs";
 import path from "path"
+import * as helpers from "./../test/testHelpers"
 dotenv.config();
 
 async function main() {
@@ -28,7 +29,7 @@ async function main() {
   //feed with link
   let gasPrice = await provider.getGasPrice();
   let gasPriceWei=gasPrice.toNumber();
-  let initialLinkAmount = 5;
+  let initialLinkAmount = 6;
   let signer = new ethers.Wallet(process.env.PRIVATE_KEY as string,provider);
   let linkContAddress=(name=='kovan')?confs.LINK_ADDR_KOVAN:confs.LINK_ADDR_RINKEBY;
   let LINKCont = new ethers.Contract(linkContAddress,confs.LINK_ABI, signer);
@@ -74,11 +75,53 @@ async function main() {
   console.log(`path to write is ${confPathApi}`);
   try {
     fs.writeFileSync(confPathApi,contractString);
-  }catch(err) {
-    console.error("failed to write to file");
+  }catch(err: any) {
+    console.error("failed to write to file", err.message);
     return;
   }
   console.log(`successfully wrote contract details to  ${confPathApi}`);
+
+  //init with some appraisals
+  let expertSigner2 = new ethers.Wallet( process.env.EXPERT_2 as string,provider);
+  let NFTDContExp = NFTDao.connect(expertSigner2);
+  let jane = new ethers.Wallet( process.env.JANE as string,provider);
+  let NFTDContJane = NFTDao.connect(jane);
+  console.log(`initializing appraisal requests`);
+  try {
+  await helpers.submitAppraisal(NFTDContExp,
+    "4100080038",
+    "0x90709c29b779e5228e86ff04bb60a3643ea3e0e6",
+    "https://opensea.io/assets/0x90709c29b779e5228e86ff04bb60a3643ea3e0e6/4100080038",
+    6,
+    6000,
+    gasPriceWei,
+    2000000000000000);
+
+  await helpers.submitAppraisal(NFTDContExp,
+    "3900020202",
+    "0xa2d544196fdbc9da15a701c38bb60513c1802ef4",
+    "https://opensea.io/assets/0xa2d544196fdbc9da15a701c38bb60513c1802ef4/3900020202",
+    6,
+    6000,
+    gasPriceWei,
+    2000000000000000);
+      
+  await helpers.submitAppraisal(NFTDContExp,
+    "3824",
+    "0xa406489360a47af2c74fc1004316a64e469646a5",
+    "https://opensea.io/assets/0xa406489360a47af2c74fc1004316a64e469646a5/3824",
+    6,
+    6000,
+    gasPriceWei,
+    2000000000000000);
+    console.log(`done initializing appraisal requests`);  
+  } catch(error: any) {
+    console.error("failed to initialize appraisal request",error.message);
+  }
+  console.log(`initializing jane as expert`);
+  //initialize one dao member
+  await helpers.joinDao(NFTDContJane,gasPriceWei);
+  console.log(`done initializing jane as expert`);
   
 }
 
